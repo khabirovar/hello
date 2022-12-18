c92ec8bcc66696cb2097857e51218b02060be20d
username <username@think.ibs.root.ibscorp.ru>
2022-12-18 18:10:40 +0600

    Return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -18,6 +18,25 @@ func Hello(name string) (string, error) {
     return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+    // A map to associate names with messages.
+    messages := make(map[string]string)
+    // Loop through the recieved slice names, calling
+    // the Hello function to get a message for each name.
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        // In the map, associate the retrieved message with 
+        // the name.
+        messages[name] = message
+    }
+    return messages, nil
+}
+
 // init sets initial values for variables used in function.
 func init() {
     rand.Seed(time.Now().UnixNano())
@@ -28,9 +47,9 @@ func init() {
 func randomFormat() string {
     // A slice of message formats.
     formats := []string{
-        "Hi, %v. Welcome!",
-	"Great to see you, %v!",
-	"Hello, %v! Well met!",
+      "Hi, %v. Welcome!",
+	    "Great to see you, %v!",
+	    "Hello, %v! Well met!",
     }
 
     // Return a randomly selected message format
--- hello.go
+++ hello.go
@@ -14,8 +14,11 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
     // Request a greeting message.
-    message, err := greetings.Hello("Gladys!")
+    messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
@@ -24,5 +27,5 @@ func main() {
 
     // If no error was returned, print the returned message
     // to the console.
-    fmt.Println(message)
+    fmt.Println(messages)
 }
